75cc4c6c519fdceed54cfdb35b1c26d05924b236
Manish Kumar <76213954+Manishk1832@users.noreply.github.com>
2023-05-31 19:47:52 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,46 @@
+// const axios = require("axios")
+
+
+const URL = "https://api.tvmaze.com/search/shows?q=";
+const form = document.getElementById("form");
+const result = document.getElementById("container");
+
+function removemovies(parent) {
+    while (parent.firstChild) {
+
+        parent.firstChild.remove();
+    }
+}
+
+
+
+
+function getMovies(searchtext) {
+    axios.get(`${URL} ${searchtext}`)
+        .then((res) => {
+            const movies = res.data;
+            //console.log(res.data)
+            for (let movie of movies) {
+                //console.log(movies)
+                if (movie.show.image != null) {
+                    const image = document.createElement("img");
+                    image.setAttribute("src", movie.show.image.medium);
+                    result.append(image);
+                }
+            }
+        })
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchtext = form.children[1].value;
+    console.log(searchtext);
+    removemovies(result);
+
+    getMovies(searchtext);
+    form.children[1].value = " "
+})
+
+
